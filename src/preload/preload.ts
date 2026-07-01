a2fd1a23990d9420bf155ed4b5b69648f1392ed4
anikrauth <anikrouthwebdev@gmail.com>
2026-07-02 02:18:43 +0600

    feat: thread history param through preload sendPrompt bridge

--- src/preload/preload.ts
+++ src/preload/preload.ts
@@ -2,7 +2,7 @@ import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('electron', {
   selectFolder: () => ipcRenderer.invoke('dialog:openDirectory'),
-  sendPrompt: (prompt: string, workspace: string, settings: any) => ipcRenderer.send('agent:prompt', prompt, workspace, settings),
+  sendPrompt: (prompt: string, workspace: string, settings: any, history: any) => ipcRenderer.send('agent:prompt', prompt, workspace, settings, history),
   onAgentEvent: (callback: (event: any) => void) => {
     ipcRenderer.removeAllListeners('agent:event');
     ipcRenderer.on('agent:event', (_event, value) => callback(value));
